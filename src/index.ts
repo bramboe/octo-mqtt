c1a5f63472dc7c1ce5d3c3f3f3b94c961d86f9aa
bramboe <37293908+bramboe@users.noreply.github.com>
2025-05-07 10:44:13 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import type { IMQTTConnection } from '@mqtt/IMQTTConnection';
 import { connectToMQTT } from '@mqtt/connectToMQTT';
 import { loadStrings } from '@utils/getString';
 import { logError, logWarn } from '@utils/logger';
-import { getType } from '@utils/options';
 import { connectToESPHome } from 'ESPHome/connectToESPHome';
 import { octo } from 'Octo/octo';
-import { scanner } from 'Scanner/scanner';
 
 const processExit = (exitCode?: number) => {
   if (exitCode && exitCode > 0) {
@@ -15,7 +13,7 @@ const processExit = (exitCode?: number) => {
 };
 
 process.on('exit', () => {
-  logWarn('Shutting down Smartbed-MQTT...');
+  logWarn('Shutting down Octo-MQTT...');
   processExit(0);
 });
 process.on('SIGINT', () => processExit(0));
@@ -29,28 +27,14 @@ const start = async () => {
   await loadStrings();
 
   const mqtt: IMQTTConnection = await connectToMQTT();
-
-  // http/udp
-  // switch (getType()) {
-    // case 'sleeptracker':
-    //   return void (await sleeptracker(mqtt));
-    // case 'ergowifi':
-    //   return void (await ergowifi(mqtt));
-    // case 'logicdata':
-    //   return void (await logicdata(mqtt));
-    // case 'ergomotion':
-    //   return void (await ergomotion(mqtt));
-  // }
-  // bluetooth
   const esphome = await connectToESPHome();
-  switch (getType()) {
-    case 'octo':
-      return void (await octo(mqtt, esphome));
-    case 'scanner':
-      return void (await scanner(esphome));
-    default:
-      logError(`Unsupported bed type: ${getType()}`);
-      processExit(1);
+  
+  try {
+    await octo(mqtt, esphome);
+  } catch (error) {
+    logError('Failed to initialize Octo MQTT:', error);
+    processExit(1);
   }
 };
+
 void start();
