7b467188e1e7ecb3f02d5e8c5aac4b037f85709e
bramboe <37293908+bramboe@users.noreply.github.com>
2025-05-27 15:39:33 +0200

    Delete octo-mqtt-main-old directory

--- octo-mqtt-main-old/octo-mqtt/src/HomeAssistant/base/IStateful.ts
+++
@@ -1,4 +0,0 @@
-export interface IStateful<T> {
-  setState(state: T | null): void;
-  getState(): T | undefined;
-}
--- octo-mqtt-main-old/octo-mqtt/src/MQTT/IMQTTConnection.ts
+++
@@ -1,8 +0,0 @@
-export interface IMQTTConnection {
-  unsubscribe(topic: string): void;
-  on(event: string, listener: (this: IMQTTConnection, message: string) => void): this;
-  off(event: string, listener: (this: IMQTTConnection, message: string) => void): this;
-  publish(topic: string, message: any): void;
-  subscribe(topic: string): void;
-  disconnect(): Promise<void>;
-}
